89c05bb5147f3fda200d2a66866de800add71c2e
Anas TABITI <atabiti@e1r3p1.1337.ma>
2022-12-25 16:22:20 +0100

    f

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 11:00:56 by atabiti           #+#    #+#             */
-/*   Updated: 2022/12/25 15:05:32 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/12/25 16:22:10 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,15 @@
 int main()
 {
 	ft::vector<int> fake(42);
-	ft::vector<int>::value_type f;
-	std::vector<int>::value_type f1;
-	
 	std::vector<int> real(42);
-	
-	std::cout << f << std::endl;
-	std::cout << f1<< std::endl;
-
-
-	
-	// std::cout << "fake SIZE=" << fake.size() << std::endl;
-	// std::cout << "real SIZE=" << real.size() << std::endl;
-	// std::cout << "fake capacity=" << fake.capacity() << std::endl;
-	// std::cout << "real capacity=" << real.capacity() << std::endl;
-	// fake.push_back(1337);
-	// std::cout << fake.at(42) << std::endl;
-	// real.push_back(42);
-	// std::cout << real.at(42) << std::endl;
+	std::vector<int>::value_type typ = real[0];
+	std::cout << typ << std::endl;
+	std::cout << "fake SIZE=" << fake.size() << std::endl;
+	std::cout << "real SIZE=" << real.size() << std::endl;
+	std::cout << "fake capacity=" << fake.capacity() << std::endl;
+	std::cout << "real capacity=" << real.capacity() << std::endl;
+	fake.push_back(1337);
+	std::cout << fake.at(42) << std::endl;
+	real.push_back(42);
+	std::cout << real.at(42) << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 11:00:51 by atabiti           #+#    #+#             */
-/*   Updated: 2022/12/25 15:01:36 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/12/25 16:22:01 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,19 @@ namespace ft
 {
 
     template <typename T, typename Allocator = std::allocator<T> >
-    class vector 
+    class vector
     {
-    private:
+     private:
         T *vec;
         size_t size_param;
         size_t capacity_param;
         Allocator mine;
         /* data */
     public:
-    /* The type stored in the container. */
-    typedef T                                     value_type;
-        vector(int nm)
+        /* The type stored in the container. */
+        typedef T value_type;
+    /*______________________________________________________________________________________________________ */
+        vector(size_t nm)
         {
             size_param = nm;
             capacity_param = nm;
@@ -36,31 +37,36 @@ namespace ft
             vec = mine.allocate(nm);
             // this->vec = new T[nm];
         }
+    /*______________________________________________________________________________________________________ */
         ~vector()
         {
             mine.deallocate(vec, this->capacity_param);
             // delete[] vec;
             std::cout << "destructor is called" << std::endl;
         }
+    /*______________________________________________________________________________________________________ */
         size_t size()
         {
             // return (mine.max_size());
             return (this->size_param);
         }
+    /*______________________________________________________________________________________________________ */
         size_t capacity()
         {
             return (this->capacity_param);
         }
 
+    /*______________________________________________________________________________________________________ */
         void push_back(T nb)
         {
             vec[size_param] = nb;
         }
-
-        T at(int nb)
+    /*______________________________________________________________________________________________________*/
+        T at(size_t nb)
         {
             return (vec[nb]);
         }
+    /*______________________________________________________________________________________________________ */
     };
 
 }
