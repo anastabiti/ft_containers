8537939abde019912c2eb7724dd7bda9ff34f80c
Anas TABITI <atabiti@e1r3p1.1337.ma>
2022-12-25 13:40:42 +0100

    d

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 11:00:56 by atabiti           #+#    #+#             */
-/*   Updated: 2022/12/25 13:38:21 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/12/25 13:40:27 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ int main()
 {
 	ft::vector<int> fake(2);
 	std::vector<int> real(42);
-	// std::cout <<"fake SIZE="<<  fake.size() << std::endl;
-	// std::cout<<"real SIZE=" <<  real.size() << std::endl;
-	// std::cout <<"fake capacity="<<  fake.capacity() << std::endl;
-	// std::cout <<"real capacity="<<  real.capacity() << std::endl;
-	// fake.push_back(1337);
-	// std::cout << fake.at(42) << std::endl;
+	std::cout <<"fake SIZE="<<  fake.size() << std::endl;
+	std::cout<<"real SIZE=" <<  real.size() << std::endl;
+	std::cout <<"fake capacity="<<  fake.capacity() << std::endl;
+	std::cout <<"real capacity="<<  real.capacity() << std::endl;
+	fake.push_back(1337);
+	std::cout << fake.at(42) << std::endl;
 	real.push_back(42);
 	std::cout << real.at(42) << std::endl;
 
