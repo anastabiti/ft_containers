27eb6b09d910bb2d19ee03ede6a18adde12622c9
Anas TABITI <atabiti@e1r3p1.1337.ma>
2022-12-25 15:05:44 +0100

    f

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 11:00:56 by atabiti           #+#    #+#             */
-/*   Updated: 2022/12/25 13:40:27 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/12/25 15:05:32 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,129 +17,23 @@
 
 int main()
 {
-	ft::vector<int> fake(2);
+	ft::vector<int> fake(42);
+	ft::vector<int>::value_type f;
+	std::vector<int>::value_type f1;
+	
 	std::vector<int> real(42);
-	std::cout <<"fake SIZE="<<  fake.size() << std::endl;
-	std::cout<<"real SIZE=" <<  real.size() << std::endl;
-	std::cout <<"fake capacity="<<  fake.capacity() << std::endl;
-	std::cout <<"real capacity="<<  real.capacity() << std::endl;
-	fake.push_back(1337);
-	std::cout << fake.at(42) << std::endl;
-	real.push_back(42);
-	std::cout << real.at(42) << std::endl;
+	
+	std::cout << f << std::endl;
+	std::cout << f1<< std::endl;
 
-	// // system("leaks ft_containers");
+
+	
+	// std::cout << "fake SIZE=" << fake.size() << std::endl;
+	// std::cout << "real SIZE=" << real.size() << std::endl;
+	// std::cout << "fake capacity=" << fake.capacity() << std::endl;
+	// std::cout << "real capacity=" << real.capacity() << std::endl;
+	// fake.push_back(1337);
+	// std::cout << fake.at(42) << std::endl;
+	// real.push_back(42);
+	// std::cout << real.at(42) << std::endl;
 }
-// #include <deque>
-// #if 1 //CREATE A REAL STL EXAMPLE
-// 	#include <map>
-// 	#include <stack>
-// 	#include <vector>
-// 	namespace ft = std;
-// #else
-// 	#include <map.hpp>
-// 	#include <stack.hpp>
-// 	#include <vector.hpp>
-// #endif
-
-// #include <stdlib.h>
-
-// #define MAX_RAM 4294967296
-// #define BUFFER_SIZE 4096
-// struct Buffer
-// {
-// 	int idx;
-// 	char buff[BUFFER_SIZE];
-// };
-
-// #define COUNT (MAX_RAM / (int)sizeof(Buffer))
-
-// template<typename T>
-// class MutantStack : public ft::stack<T>
-// {
-// public:
-// 	MutantStack() {}
-// 	MutantStack(const MutantStack<T>& src) { *this = src; }
-// 	MutantStack<T>& operator=(const MutantStack<T>& rhs)
-// 	{
-// 		this->c = rhs.c;
-// 		return *this;
-// 	}
-// 	~MutantStack() {}
-
-// 	typedef typename ft::stack<T>::container_type::iterator iterator;
-
-// 	iterator begin() { return this->c.begin(); }
-// 	iterator end() { return this->c.end(); }
-// };
-
-// int main(int argc, char** argv) {
-// 	if (argc != 2)
-// 	{
-// 		std::cerr << "Usage: ./test seed" << std::endl;
-// 		std::cerr << "Provide a seed please" << std::endl;
-// 		std::cerr << "Count value:" << COUNT << std::endl;
-// 		return 1;
-// 	}
-// 	const int seed = atoi(argv[1]);
-// 	srand(seed);
-
-// 	ft::vector<std::string> vector_str;
-// 	ft::vector<int> vector_int;
-// 	ft::stack<int> stack_int;
-// 	ft::vector<Buffer> vector_buffer;
-// 	ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
-// 	ft::map<int, int> map_int;
-
-// 	for (int i = 0; i < COUNT; i++)
-// 	{
-// 		vector_buffer.push_back(Buffer());
-// 	}
-
-// 	for (int i = 0; i < COUNT; i++)
-// 	{
-// 		const int idx = rand() % COUNT;
-// 		vector_buffer[idx].idx = 5;
-// 	}
-// 	ft::vector<Buffer>().swap(vector_buffer);
-
-// 	try
-// 	{
-// 		for (int i = 0; i < COUNT; i++)
-// 		{
-// 			const int idx = rand() % COUNT;
-// 			vector_buffer.at(idx);
-// 			std::cerr << "Error: THIS VECTOR SHOULD BE EMPTY!!" <<std::endl;
-// 		}
-// 	}
-// 	catch(const std::exception& e)
-// 	{
-// 		//NORMAL ! :P
-// 	}
-
-// 	for (int i = 0; i < COUNT; ++i)
-// 	{
-// 		map_int.insert(ft::make_pair(rand(), rand()));
-// 	}
-
-// 	int sum = 0;
-// 	for (int i = 0; i < 10000; i++)
-// 	{
-// 		int access = rand();
-// 		sum += map_int[access];
-// 	}
-// 	std::cout << "should be constant with the same seed: " << sum << std::endl;
-
-// 	{
-// 		ft::map<int, int> copy = map_int;
-// 	}
-// 	MutantStack<char> iterable_stack;
-// 	for (char letter = 'a'; letter <= 'z'; letter++)
-// 		iterable_stack.push(letter);
-// 	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
-// 	{
-// 		std::cout << *it;
-// 	}
-// 	std::cout << std::endl;
-// 	return (0);
-// }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 11:00:51 by atabiti           #+#    #+#             */
-/*   Updated: 2022/12/25 13:40:09 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/12/25 15:01:36 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,53 @@
 #include <string>
 namespace ft
 {
-    
 
-template <typename T, typename Allocator = std::allocator<T>>
-class vector
-{
-  private:
-	T *vec;
-	size_t size_param;
-	size_t capacity_param;
-	Allocator mine;
-	/* data */
-  public:
-	vector(int nm)
-	{
-		size_param = nm;
-		capacity_param = nm;
-		// this->vec = this->allocate ;
-		// this->allocate
-		vec = mine.allocate(nm);
-		// this->vec = new T[nm];
-	}
-	~vector()
-	{
-		mine.deallocate(vec, this->capacity_param);
-		// delete[] vec;
-		std::cout << "destructor is called" << std::endl;
-	}
-	size_t size()
-	{
-		// return (mine.max_size());
-		return (this->size_param);
-	}
-	size_t capacity()
-	{
-		return (this->capacity_param);
-	}
+    template <typename T, typename Allocator = std::allocator<T> >
+    class vector 
+    {
+    private:
+        T *vec;
+        size_t size_param;
+        size_t capacity_param;
+        Allocator mine;
+        /* data */
+    public:
+    /* The type stored in the container. */
+    typedef T                                     value_type;
+        vector(int nm)
+        {
+            size_param = nm;
+            capacity_param = nm;
+            // this->vec = this->allocate ;
+            // this->allocate
+            vec = mine.allocate(nm);
+            // this->vec = new T[nm];
+        }
+        ~vector()
+        {
+            mine.deallocate(vec, this->capacity_param);
+            // delete[] vec;
+            std::cout << "destructor is called" << std::endl;
+        }
+        size_t size()
+        {
+            // return (mine.max_size());
+            return (this->size_param);
+        }
+        size_t capacity()
+        {
+            return (this->capacity_param);
+        }
 
-	void push_back(T nb)
-	{
-		vec[size_param] = nb;
-	}
+        void push_back(T nb)
+        {
+            vec[size_param] = nb;
+        }
 
-	T at(int nb)
-	{
-		return (vec[nb]);
-	}
-};
+        T at(int nb)
+        {
+            return (vec[nb]);
+        }
+    };
 
 }
