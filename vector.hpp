9cf382cb4f55fc3f8db2ab1a37452309b04f9268
Anas TABITI <atabiti@e1r4p1.1337.ma>
2023-01-14 12:25:35 +0100

    f

--- vector.hpp
+++ vector.hpp
@@ -13,149 +13,151 @@
 #include <iostream>
 #include <string>
 // #include <memory>
-
+#include <cstddef>
 namespace ft
 {
+template <typename T, typename Allocator = std::allocator<T>>
+class vector
+{
+  private:
+	T *vec;
+	T *copY;
+	size_t size_param;
+	size_t capacity_param;
+	Allocator mine;
+	/* data */
+  public:
+	/*________________________________Member types________________________________*/
 
-    template <typename T, typename Allocator = std::allocator<T> >
-    class vector
-    {
-    private:
-        T *vec;
-        T *copY;
-        size_t size_param;
-        size_t capacity_param;
-        Allocator mine;
-        /* data */
-    public:
-    /*________________________________Member types________________________________*/
+	typedef T value_type;             /* The type stored in the container. */
+	typedef Allocator allocator_type; /*   The type of the allocator.
+	/ defaults to: allocator<value_type>   */
+	typedef value_type &reference;   
+	/* A reference to the type stored in the container. 
+	/for the default allocator: value_type& */
+	typedef const T &const_reference;
+	/*A constant reference to the type stored in the container. */
+	typedef T *pointer;
+	typedef const T *const_pointer;
+	typedef T *iterator;                   /* An iterator for the container. */
+	typedef const iterator const_iterator;
+	/* A constant iterator for the container. */
+	typedef T *reverse_iterator;
+	typedef const T *const_reverse_iterator;
+	typedef ptrdiff_t difference_type;
 
-    typedef T    value_type; /* The type stored in the container. */
-    typedef Allocator  allocator_type;     /*   The type of the allocator. / defaults to: allocator<value_type>   */
-    typedef value_type&              reference; /* A reference to the type stored in the container.  /for the default allocator: value_type& */
-    typedef const T&        const_reference; /*A constant reference to the type stored in the container. */
-    typedef T*               pointer;
-    typedef      const T*     const_pointer;
-    typedef T*              iterator; /* An iterator for the container. */
-    typedef         const iterator    const_iterator; /* A constant iterator for the container. */
-    typedef T*       reverse_iterator;
-    typedef  const T*  const_reverse_iterator;
-    typedef ptrdiff_t       difference_type; 
-
-/*       difference_type: What type results from writing it1 - it2? : 
+	/*       difference_type: What type results from writing it1 - it2? : 
          A signed integral type that can represent the difference in two iterators. 
             Result of pointer subtraction
+            ptrdiff_t = This is the signed integer type of the result of subtracting two pointers.
 */
+	typedef size_t size_type;
 
-    typedef size_t                size_type;
+	// typedef typename allocator_type::difference_type       difference_type;
+	/*______________________________________________________________________________________________________ */
 
-    // typedef typename allocator_type::difference_type       difference_type;
-/*______________________________________________________________________________________________________ */
+	/**_**_**_**_**_**_**_* Member functions  **_**_**_**_**_**_**_**_**_**_*/
 
-/**_**_**_**_**_**_**_* Member functions  *_**_**_**_**_**_**_**_**_**_*/
+	/* default constructor */
+	explicit vector(const allocator_type &alloc = allocator_type())
+		/*Constructs an empty container, with no elements.*/
+	{
+		// std::cout << "alloc " << sizeof(alloc)<<std::endl;
+		size_param = 0;
+		capacity_param = 0;
+		this->vec = this->allocate;
+		this->allocate
+			vec = mine.allocate(0);
+		// this->vec = new T[nm];
+	}
+	/*______________________________________________________________________________________________________ */
+	vector(size_type nm)
+	{
+		size_param = nm;
+		capacity_param = nm;
+		// this->vec = this->allocate ;
+		// this->allocate
+		vec = mine.allocate(nm);
+		// this->vec = new T[nm];
+	}
+	/*______________________________________________________________________________________________________ */
+	vector(vector const &rhs)
+	{
+		*this = rhs;
+	}
+	/*______________________________________________________________________________________________________ */
+	vector &operator=(vector const &rhs)
+	{
+		size_t i = 0;
+		while (i < this->s)
+		{
+			this->vec[i] = rhs.vec[i];
+			i++;
+		}
+		this->vec = rhs.vec;
 
-        explicit vector (const allocator_type& alloc = allocator_type())
-        {
-            std::cout << "alloc " << sizeof(alloc)<<std::endl;
-        }
-        // vector()
-        // {
-        //     size_param = 0;
-        //     capacity_param = 0;
-        //     // this->vec = this->allocate ;
-        //     // this->allocate
-        //     // vec = mine.allocate(0);
-        //     // this->vec = new T[nm];
-        // }
-        /*______________________________________________________________________________________________________ */
-        /*______________________________________________________________________________________________________ */
-        vector(size_type nm)
-        {
-            size_param = nm;
-            capacity_param = nm;
-            // this->vec = this->allocate ;
-            // this->allocate
-            vec = mine.allocate(nm);
-            // this->vec = new T[nm];
-        }
-        /*______________________________________________________________________________________________________ */
-        vector(vector const &rhs)
-        {
-           *this = rhs;
-        }
-        /*______________________________________________________________________________________________________ */
-        vector &operator=(vector const &rhs)
-        {
-                size_t i = 0;
-                while (i < this->s)
-                {
-                    this->vec[i] = rhs.vec[i];
-                    i++;
-                }
-                this->vec = rhs.vec;
-                
-            return *this;
-        }
-        /*______________________________________________________________________________________________________ */
-        ~vector()
-        {
-            mine.deallocate(vec, this->capacity_param);
-            std::cout << "destructor is called" << std::endl;
-        }
-        /*______________________________________________________________________________________________________ */
-        size_type size()
-        {
-            // return (mine.max_size());
-            return (this->size_param);
-        }
-        /*______________________________________________________________________________________________________ */
-        size_type capacity()
-        {
-            return (this->capacity_param);
-        }
+		return (*this);
+	}
+	/*______________________________________________________________________________________________________ */
+	~vector()
+	{
+		mine.deallocate(vec, this->capacity_param);
+		std::cout << "destructor is called" << std::endl;
+	}
+	/*______________________________________________________________________________________________________ */
+	size_type size()
+	{
+		// return (mine.max_size());
+		return (this->size_param);
+	}
+	/*______________________________________________________________________________________________________ */
+	size_type capacity()
+	{
+		return (this->capacity_param);
+	}
 
-        /*______________________________________________________________________________________________________ */
-        void push_back(value_type const &nb)
-        {
-            // start with nothing case	ft::vector<int> fake;
-            if (size_param == 0)
-            {
-                vec = mine.allocate(1);
-                capacity_param = 1;
-                size_param = 0;
-                vec[size_param] = nb;
-                size_param++;
-                return;
-            }
-            else if (size_param > 0 && size_param < capacity_param)
-            {
-                vec[size_param] = nb;
-                ++size_param;
-                return;
-            }
-            else if (size_param == capacity_param)
-            {
-                capacity_param = capacity_param * 2;
-                copY = mine.allocate(capacity_param);
-                size_t i = 0;
-                while (i < size_param) 
-                {
-                    copY[i] = vec[i];
-                    i++;
-                }
-                copY[i] = nb;
-                mine.deallocate(vec,capacity_param);
-                vec = copY;
-                size_param++;
-                return;
-            }
-        }
-        /*______________________________________________________________________________________________________*/
-        value_type at(size_type nb)
-        {
-            return (vec[nb]);
-        }
-        /*______________________________________________________________________________________________________ */
-    };
+	/*______________________________________________________________________________________________________ */
+	void push_back(value_type const &nb)
+	{
+		// start with nothing case	ft::vector<int> fake;
+		if (size_param == 0)
+		{
+			vec = mine.allocate(1);
+			capacity_param = 1;
+			size_param = 0;
+			vec[size_param] = nb;
+			size_param++;
+			return ;
+		}
+		else if (size_param > 0 && size_param < capacity_param)
+		{
+			vec[size_param] = nb;
+			++size_param;
+			return ;
+		}
+		else if (size_param == capacity_param)
+		{
+			capacity_param = capacity_param * 2;
+			copY = mine.allocate(capacity_param);
+			size_t i = 0;
+			while (i < size_param)
+			{
+				copY[i] = vec[i];
+				i++;
+			}
+			copY[i] = nb;
+			mine.deallocate(vec, capacity_param);
+			vec = copY;
+			size_param++;
+			return ;
+		}
+	}
+	/*______________________________________________________________________________________________________*/
+	value_type at(size_type nb)
+	{
+		return (vec[nb]);
+	}
+	/*______________________________________________________________________________________________________ */
+};
 
-}
+} // namespace ft
