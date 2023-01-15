d666dc0a47823b143caf7a5e353f564c5ffaeeba
Anas TABITI <atabiti@e1r4p1.1337.ma>
2023-01-15 09:48:41 +0100

    f

--- vector.hpp
+++ vector.hpp
@@ -26,7 +26,7 @@ class vector
 	T *copY;
 	size_t size_param;
 	size_t capacity_param;
-	Allocator mine;
+	// Allocator mine;
 	/* data */
   public:
 	/*________________________________Member types________________________________*/
@@ -60,7 +60,6 @@ class vector
 	/* Constructs an empty container, with no elements.*/
 
 	explicit vector(const allocator_type &alloc = allocator_type())
-	// explicit vector()
 	{
 		size_param = 0;
 		capacity_param = 0;
@@ -85,93 +84,93 @@ class vector
 
 
 
-	/*______________________________________________________________________________________________________ */
-	vector(size_type nm)
-	{
-		size_param = nm;
-		capacity_param = nm;
-		// this->vec = this->allocate ;
-		// this->allocate
-		vec = mine.allocate(nm);
-		// this->vec = new T[nm];
-	}
-	/*______________________________________________________________________________________________________ */
-	vector(vector const &rhs)
-	{
-		*this = rhs;
-	}
-	/*______________________________________________________________________________________________________ */
-	vector &operator=(vector const &rhs)
-	{
-		size_t i = 0;
-		while (i < this->s)
-		{
-			this->vec[i] = rhs.vec[i];
-			i++;
-		}
-		this->vec = rhs.vec;
+	// /*______________________________________________________________________________________________________ */
+	// vector(size_type nm)
+	// {
+	// 	size_param = nm;
+	// 	capacity_param = nm;
+	// 	// this->vec = this->allocate ;
+	// 	// this->allocate
+	// 	vec = mine.allocate(nm);
+	// 	// this->vec = new T[nm];
+	// }
+	// /*______________________________________________________________________________________________________ */
+	// vector(vector const &rhs)
+	// {
+	// 	*this = rhs;
+	// }
+	// /*______________________________________________________________________________________________________ */
+	// vector &operator=(vector const &rhs)
+	// {
+	// 	size_t i = 0;
+	// 	while (i < this->s)
+	// 	{
+	// 		this->vec[i] = rhs.vec[i];
+	// 		i++;
+	// 	}
+	// 	this->vec = rhs.vec;
 
-		return (*this);
-	}
-	/*______________________________________________________________________________________________________ */
-	~vector()
-	{
-		mine.deallocate(vec, this->capacity_param);
-		std::cout << "destructor is called" << std::endl;
-	}
-	/*______________________________________________________________________________________________________ */
-	size_type size()
-	{
-		// return (mine.max_size());
-		return (this->size_param);
-	}
-	/*______________________________________________________________________________________________________ */
-	size_type capacity()
-	{
-		return (this->capacity_param);
-	}
+	// 	return (*this);
+	// }
+	// /*______________________________________________________________________________________________________ */
+	// ~vector()
+	// {
+	// 	mine.deallocate(vec, this->capacity_param);
+	// 	std::cout << "destructor is called" << std::endl;
+	// }
+	// /*______________________________________________________________________________________________________ */
+	// size_type size()
+	// {
+	// 	// return (mine.max_size());
+	// 	return (this->size_param);
+	// }
+	// /*______________________________________________________________________________________________________ */
+	// size_type capacity()
+	// {
+	// 	return (this->capacity_param);
+	// }
 
-	/*______________________________________________________________________________________________________ */
-	void push_back(value_type const &nb)
-	{
-		// start with nothing case	ft::vector<int> fake;
-		if (size_param == 0)
-		{
-			vec = mine.allocate(1);
-			capacity_param = 1;
-			size_param = 0;
-			vec[size_param] = nb;
-			size_param++;
-			return ;
-		}
-		else if (size_param > 0 && size_param < capacity_param)
-		{
-			vec[size_param] = nb;
-			++size_param;
-			return ;
-		}
-		else if (size_param == capacity_param)
-		{
-			capacity_param = capacity_param * 2;
-			copY = mine.allocate(capacity_param);
-			size_t i = 0;
-			while (i < size_param)
-			{
-				copY[i] = vec[i];
-				i++;
-			}
-			copY[i] = nb;
-			mine.deallocate(vec, capacity_param);
-			vec = copY;
-			size_param++;
-			return ;
-		}
-	}
-	/*______________________________________________________________________________________________________*/
-	value_type at(size_type nb)
-	{
-		return (vec[nb]);
-	}
+	// /*______________________________________________________________________________________________________ */
+	// void push_back(value_type const &nb)
+	// {
+	// 	// start with nothing case	ft::vector<int> fake;
+	// 	if (size_param == 0)
+	// 	{
+	// 		vec = mine.allocate(1);
+	// 		capacity_param = 1;
+	// 		size_param = 0;
+	// 		vec[size_param] = nb;
+	// 		size_param++;
+	// 		return ;
+	// 	}
+	// 	else if (size_param > 0 && size_param < capacity_param)
+	// 	{
+	// 		vec[size_param] = nb;
+	// 		++size_param;
+	// 		return ;
+	// 	}
+	// 	else if (size_param == capacity_param)
+	// 	{
+	// 		capacity_param = capacity_param * 2;
+	// 		copY = mine.allocate(capacity_param);
+	// 		size_t i = 0;
+	// 		while (i < size_param)
+	// 		{
+	// 			copY[i] = vec[i];
+	// 			i++;
+	// 		}
+	// 		copY[i] = nb;
+	// 		mine.deallocate(vec, capacity_param);
+	// 		vec = copY;
+	// 		size_param++;
+	// 		return ;
+	// 	}
+	// }
+	// /*______________________________________________________________________________________________________*/
+	// value_type at(size_type nb)
+	// {
+	// 	return (vec[nb]);
+	// }
 	/*______________________________________________________________________________________________________ */
 };
 
