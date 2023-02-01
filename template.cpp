136f9b4177fc5e55a363da50b70272b6e0b41de6
Anas TABITI <atabiti@e1r3p1.1337.ma>
2023-02-01 10:34:12 +0100

    working on insert

--- template.cpp
+++ template.cpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 09:33:11 by atabiti           #+#    #+#             */
-/*   Updated: 2023/02/01 10:29:56 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/02/01 10:34:08 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 template <typename T>
  struct is_integral_impl
 {
-    static const bool value = false;
+   static   const bool value = false; //in-class initialization of non-static data member is a C++11 extension with no static
 };
 template<>
 struct is_integral_impl<int>
 {
-    static const bool value = true;
+   static  const bool value = true;
     
 };
 // template<>
@@ -33,7 +33,7 @@ struct is_integral_impl<int>
 
 template <typename T>
 struct is_integral {
-    static const bool value = is_integral_impl<typename std::remove_cv<T>::type>::value;
+      static const bool value = is_integral_impl<typename std::remove_cv<T>::type>::value;
     };
 
 
@@ -68,6 +68,8 @@ int main()
 {
     std::string f("SsSS");
     std::vector<short int> f1(1,4);
+    short  x = 3333;
     // std::cout << foo(f) << std::endl;
-    std::cout << foo(f1.at(0)) << std::endl;
+    std::cout << foo(x) << std::endl;
+    // std::cout << foo(f1.at(0)) << std::endl;
 }
