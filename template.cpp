adfe931849eed1ebb962f533fec2f0f09a0f5595
Anas TABITI <atabiti@e1r3p1.1337.ma>
2023-02-01 10:39:25 +0100

    working on insert

--- template.cpp
+++ template.cpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 09:33:11 by atabiti           #+#    #+#             */
-/*   Updated: 2023/02/01 10:34:08 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/02/01 10:39:22 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,77 @@ template <typename T>
    static   const bool value = false; //in-class initialization of non-static data member is a C++11 extension with no static
 };
 template<>
+struct is_integral_impl<bool>
+{
+    static const bool value = true;
+};
+template<>
+struct is_integral_impl<char>
+{
+    static const bool value = true;
+};
+template<>
+struct is_integral_impl<signed char>
+{
+    static const bool value = true;
+};
+template<>
+struct is_integral_impl<unsigned char>
+{
+    static const bool value = true;
+};
+template<>
+struct is_integral_impl<wchar_t>
+{
+    static const bool value = true;
+};
+template<>
+struct is_integral_impl<short>
+{
+    static const bool value = true;
+};
+
+template<>
+struct is_integral_impl<unsigned short>
+{
+    static const bool value = true;
+};
+template<>
 struct is_integral_impl<int>
 {
    static  const bool value = true;
     
 };
+template<>
+struct is_integral_impl<unsigned int>
+{
+   static  const bool value = true;
+    
+};
+
+template<>
+struct is_integral_impl<long>
+{
+    static const bool value = true;
+};
+template<>
+struct is_integral_impl<unsigned long>
+{
+    static const bool value = true;
+};
+template<>
+struct is_integral_impl<long long>
+{
+    static const bool value = true;
+};
+template<>
+struct is_integral_impl<unsigned long long>
+{
+    static const bool value = true;
+};
+
 // template<>
-// struct is_integral_impl<short int>
+// struct is_integral_impl<short>
 // {
 //     static const bool value = true;
 // };
@@ -68,7 +132,7 @@ int main()
 {
     std::string f("SsSS");
     std::vector<short int> f1(1,4);
-    short  x = 3333;
+wchar_t  x = 'd';
     // std::cout << foo(f) << std::endl;
     std::cout << foo(x) << std::endl;
     // std::cout << foo(f1.at(0)) << std::endl;
