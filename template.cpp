0ad389a681aaa92cbd3f4afa3a37f0b669fabacd
Anas TABITI <atabiti@e1r3p1.1337.ma>
2023-02-01 09:55:07 +0100

    working on insert

--- template.cpp
+++ template.cpp
@@ -6,34 +6,43 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 09:33:11 by atabiti           #+#    #+#             */
-/*   Updated: 2023/02/01 09:41:31 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/02/01 09:52:56 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <vector>
 #include <type_traits>
+template <bool B, typename T = void>
+struct enable_if {
+    typedef T type;
+};
+ 
+template <typename T>
+struct enable_if<false, T> {};
+
 // template <typename T>
 // T foo(T g)
 // {
 //     return g;
 // }
 template <typename T>
-typename std::enable_if<std::is_integral<T>::value, T>::type
+typename enable_if<std::is_integral<T>::value, T>::type
 foo(T g)
 {
     return g;
 }
-template <typename T>
-typename std::enable_if<!std::is_integral<T>::value, T>::type
-foo(T g)
-{
-    return g;
-}
+// template <typename T>
+// typename enable_if<!std::is_integral<T>::value, T>::type
+// foo(T g)
+// {
+//     return g;
+// }
 
 int main()
 {
     std::string f("SsSS");
     std::vector<int> f1(1,4);
     std::cout << foo(f) << std::endl;
+    // std::cout << foo(f1.at(0)) << std::endl;
 }
