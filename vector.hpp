a1667108a14b46c88ab466ae8833123389cec1e5
Anas TABITI <atabiti@e1r3p1.1337.ma>
2023-02-06 09:21:51 +0100

    lexicographical_compare is done

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 11:00:51 by atabiti           #+#    #+#             */
-/*   Updated: 2023/02/06 09:20:34 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/02/06 09:21:47 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -787,7 +787,7 @@ void			swap(vector<T,Allocator>&);
 	template <class T,class Alloc>  
 	 bool operator!= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs)
 	{
-		// std::cout << "operator == vector is called"<< std::endl;
+		std::cout << "operator != vector is called"<< std::endl;
 		return !(lhs == rhs);
 	}
 
