09af4aade17371298d9daaa256d04b72a7fc6f40
Anas TABITI <atabiti@e1r3p1.1337.ma>
2023-02-09 08:35:47 +0100

    fixing segfaults 		working on insert (fill)

--- ft_containers-unit-test/sources/system/run_stack.cpp
+++ ft_containers-unit-test/sources/system/run_stack.cpp
@@ -9,18 +9,18 @@ int main(int argc, char* argv[], char* env[]) {
 	printElement("FUNCTION"); printElement(WHITE + "RESULT" + RESET); printElement(WHITE + "FT TIME" + RESET); printElement(WHITE + "STD TIME" + RESET);
 	printElement(WHITE + "LEAKS" + RESET); cout << endl;
 
-	// runFunctionTest("../stack_tests/constructor.cpp", argv, env);
-	// runFunctionTest("../stack_tests/push().cpp", argv, env);
-	// runFunctionTest("../stack_tests/top().cpp", argv, env);
-	// runFunctionTest("../stack_tests/pop().cpp", argv, env);
-	// runFunctionTest("../stack_tests/size().cpp", argv, env);
-	// runFunctionTest("../stack_tests/empty().cpp", argv, env);
-	// runFunctionTest("../stack_tests/operator ==.cpp", argv, env);
-	// runFunctionTest("../stack_tests/operator !=.cpp", argv, env);
-	// runFunctionTest("../stack_tests/operator <.cpp", argv, env);
-	// runFunctionTest("../stack_tests/operator >.cpp", argv, env);
-	// runFunctionTest("../stack_tests/operator <=.cpp", argv, env);
-	// runFunctionTest("../stack_tests/operator >=.cpp", argv, env);
+	runFunctionTest("../stack_tests/constructor.cpp", argv, env);
+	runFunctionTest("../stack_tests/push().cpp", argv, env);
+	runFunctionTest("../stack_tests/top().cpp", argv, env);
+	runFunctionTest("../stack_tests/pop().cpp", argv, env);
+	runFunctionTest("../stack_tests/size().cpp", argv, env);
+	runFunctionTest("../stack_tests/empty().cpp", argv, env);
+	runFunctionTest("../stack_tests/operator ==.cpp", argv, env);
+	runFunctionTest("../stack_tests/operator !=.cpp", argv, env);
+	runFunctionTest("../stack_tests/operator <.cpp", argv, env);
+	runFunctionTest("../stack_tests/operator >.cpp", argv, env);
+	runFunctionTest("../stack_tests/operator <=.cpp", argv, env);
+	runFunctionTest("../stack_tests/operator >=.cpp", argv, env);
 
 	saveResult("stack_res");
 	outputCurrentResult("STACK", _STACK_TEST_NUM);
