6075296180a263dd71a091a8a5dd529ab0c44a53
Anas TABITI <atabiti@e1r4p1.1337.ma>
2023-02-11 09:12:13 +0100

    fixing>

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 11:00:51 by atabiti           #+#    #+#             */
-/*   Updated: 2023/02/11 09:09:22 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/02/11 09:12:06 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -185,7 +185,16 @@ class vector
 		// }
 		// allocating.deallocate(start_iter, this->capacity_param);
 		// clear();
-		size_t dist = std::distance(first, last);
+		// size_t dist = std::distance(first, last);
+		iterator it  = first;
+		size_t dist = 0 ;
+		while (it < last)
+		{
+			it++;
+			dist++;
+		}
+		
+		
 
 		if (dist > capacity_param)
 			reserve(dist);
