58b362fa5410e6ef15b75bc1e453ac9de6846119
Anas TABITI <atabiti@e1r3p1.1337.ma>
2023-02-15 14:09:23 +0100

    add map

--- map/main.cpp
+++ map/main.cpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 12:29:31 by atabiti           #+#    #+#             */
-/*   Updated: 2023/02/15 13:03:30 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/02/15 13:08:40 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,24 @@
 int main()
 {
     // ft::vector< std::pair <int, int> > vecc;
-    ft::vector< int > vecc;
-    size_t  i = 0 ;
-    while (i< 10000000)
-    {
-        vecc.push_back( i*3);
-        // vecc.push_back( std::make_pair(i,i*3));
-        i++;
-    }
-    
-    // std::map< int, int> mapp;
-    // size_t x = 0;
-    // while (x < 10000000)
+    // std::vector< int > vecc;
+    // size_t  i = 0 ;
+    // while (i< 10000000)
     // {
-        
-    //      mapp[x] = x*3;
-    //     x++;
+    //     vecc.push_back( i*3);
+    //     // vecc.push_back( std::make_pair(i,i*3));
+    //     i++;
     // }
+    
+    std::map< int, int> mapp;
+    size_t x = 0;
+    int f = x;
+    while (x < 10000000)
+    {
+        
+         mapp[f] = f;
+        x++;
+    }
 
     // std::cout << "found " << mapp.find(121311)->second<< std::endl;
     
