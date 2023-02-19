e35a416872743b5c744f0c37220c5fa1028a0eb4
Anas TABITI <atabiti@e1r4p1.1337.ma>
2023-02-19 13:09:48 +0100

    working on tree

--- map/avl_tree.hpp
+++ map/avl_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 11:08:00 by atabiti           #+#    #+#             */
-/*   Updated: 2023/02/19 13:07:44 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/02/19 13:09:40 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,52 +69,49 @@ class avl_tree
 			return (-1);
 		else
 		{
-      
 			int rr = get_balance_height(r->right);
 			int l = get_balance_height(r->left);
-      // std::cout << "right : " <<rr<< std::endl;
-      // // std::cout << "______________________" <<rr<< std::endl;
-      
-      // std::cout << "left : " <<l<< std::endl;
-      // // std::cout << "______________________" <<rr<< std::endl;
+			// std::cout << "right : " <<rr<< std::endl;
+			// // std::cout << "______________________" <<rr<< std::endl;
+
+			// std::cout << "left : " <<l<< std::endl;
+			// // std::cout << "______________________" <<rr<< std::endl;
 			if (rr > l)
 				return (rr + 1);
 			else
 				return (l + 1);
 		}
 	}
- 	nodes<T> *left_Rotation(	nodes<T> *x)
+	nodes<T> *left_Rotation(nodes<T> *x)
 	{
-			nodes<T>  *y = x->right;
-			nodes<T>  *T2 = y->left;
-    std::cout << "root :" << *x<< std::endl;
-    std::cout << "y :" << *y<< std::endl;
-    // std::cout << "T2 :" << *T2<< std::endl;
-    
+		nodes<T> *y = x->right;
+		nodes<T> *T2 = y->left;
+		std::cout << "root :" << *x << std::endl;
+		std::cout << "y :" << *y << std::endl;
+		// std::cout << "T2 :" << *T2<< std::endl;
+
 		// Perform rotation
 		y->left = x;
 		x->right = T2;
 
 		return (y);
 	}
- 	nodes<T> *right_Rotation(	nodes<T> *x)
+	nodes<T> *right_Rotation(nodes<T> *x)
 	{
-			nodes<T>  *y = x->left;
-			nodes<T>  *T2 = y->right;
+		nodes<T> *y = x->left;
+		nodes<T> *T2 = y->right;
 
+		std::cout << "root :" << *x << std::endl;
+		std::cout << "y :" << *y << std::endl;
+		// std::cout << "T2 :" << *T2<< std::endl;
 
-      
-    std::cout << "root :" << *x<< std::endl;
-    std::cout << "y :" << *y<< std::endl;
-    // std::cout << "T2 :" << *T2<< std::endl;
-    
 		// Perform rotation
 		y->right = x;
 		x->left = T2;
 
 		return (y);
 	}
-  
+
 	nodes<T> *insert(nodes<T> *root, nodes<T> *new_node)
 	{
 		if (root == NULL)
@@ -135,67 +132,41 @@ class avl_tree
 		{
 			return (root);
 		}
-    int left_balance   = get_balance_height(root->left);
-    int right_balance   = get_balance_height(root->right);
-    int sum = left_balance - right_balance;
-    
-    // std::cout << "sum  = "<< sum << std::endl;
-    
-    if ( sum > 1 && new_node->value < root->left->value) 
-    {
-      std::cout << "right rotation"<< std::endl;  
-      // std::cout << " root : "<< *root << std::endl;  
-      		return (right_Rotation(root));
-    } 
+		int left_balance = get_balance_height(root->left);
+		int right_balance = get_balance_height(root->right);
+		int sum = left_balance - right_balance;
 
-    if ( sum < -1  && new_node->value > root->right->value) 
-    {
-      std::cout << "left rotation" << std::endl;  
-      // std::cout << " root : "<< *root << std::endl;  
-      return left_Rotation(root);
-    } 
+		// std::cout << "sum  = "<< sum << std::endl;
 
-    if ( sum  > 1  && new_node->value > root->left->value) 
-    {
-      std::cout << "left . right rotation"<< sum << std::endl; 
-      root -> left = left_Rotation(root->left);
-		  return (right_Rotation(root));
-            
-    } 
+		if (sum > 1 && new_node->value < root->left->value)
+		{
+			std::cout << "right rotation" << std::endl;
+			// std::cout << " root : "<< *root << std::endl;
+			return (right_Rotation(root));
+		}
 
-    if ( sum  < -1  && new_node->value < root->right->value) 
-    {
-      std::cout << " ( sum  < -1  && new_node->value < root->right->value)  "<< sum << std::endl;      
-      root -> right = right_Rotation(root->right);
-		  return (left_Rotation(root));
-    } 
+		if (sum < -1 && new_node->value > root->right->value)
+		{
+			std::cout << "left rotation" << std::endl;
+			// std::cout << " root : "<< *root << std::endl;
+			return (left_Rotation(root));
+		}
 
-    
-		// int bf = getBalanceFactor(r);
-		// // Left Left Case
-		// if (bf > 1 && new_node -> value < r -> left -> value)
-		//   return (right_Rotation(r));
+		if (sum > 1 && new_node->value > root->left->value)
+		{
+			std::cout << "left . right rotation" << sum << std::endl;
+			root->left = left_Rotation(root->left);
+			return (right_Rotation(root));
+		}
 
-		// // Right Right Case
-		// if (bf < -1 && new_node -> value > r -> right -> value)
-		//   return (left_Rotation(r));
-
-		// // Left Right Case
-		// if (bf > 1 && new_node -> value > r -> left -> value) {
-		//   r -> left = left_Rotation(r -> left);
-		//   return (right_Rotation(r));
-		// }
-
-		// // Right Left Case
-		// if (bf < -1 && new_node -> value < r -> right -> value) {
-		//   r -> right = right_Rotation(r -> right);
-		//   return (left_Rotation(r));
-		// }
-
-		/* return the (unchanged) node pointer */
+		if (sum < -1 && new_node->value < root->right->value)
+		{
+			std::cout << " right . left rotation" << sum << std::endl;
+			root->right = right_Rotation(root->right);
+			return (left_Rotation(root));
+		}
 		return (root);
 	}
-  
 
 	void print2D(nodes<T> *r, int space)
 	{
