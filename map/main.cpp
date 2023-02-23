e7edd0b9fc8e2f936434956770e6fdbac3c1c006
anastabiti <anastabiti@gmail.com>
2023-02-23 20:01:00 +0100

    add remove node

--- map/main.cpp
+++ map/main.cpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 12:29:31 by atabiti           #+#    #+#             */
-/*   Updated: 2023/02/23 14:50:15 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/02/23 19:59:42 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,22 @@ int main()
 {
 
     ft::map<int, int> mymap;
-    // mymap.insert(std::pair< int , int> (44, 42) );
-    // mymap.insert(std::pair< int , int> (3, 4132) );
-    // mymap.insert(std::pair< int , int> (4, 4132) );
+    mymap.insert(std::pair< int , int> (44, 42) );
+    mymap.insert(std::pair< int , int> (3, 4132) );
+    mymap.insert(std::pair< int , int> (4, 4132) );
     mymap.insert(std::pair< int , int> (1, 42) );
     mymap.insert(std::pair< int , int> (3, 4132) );
     mymap.insert(std::pair< int , int> (2, 4132) );
-    // mymap.insert(std::pair< int , int> (5, 4132) );
+    mymap.insert(std::pair< int , int> (53, 4132) );
+    mymap.insert(std::pair< int , int> (52, 4132) );
     mymap.a_tree.print2D( mymap.a_tree.root , 6);
+    std::cout  << "before ______________________________" << std::endl;
+    mymap.a_tree.remove_a_node(mymap.a_tree.root ,std::pair< int , int> (44, 42) );
+    mymap.a_tree.remove_a_node(mymap.a_tree.root ,std::pair< int , int> (3, 42) );
+    mymap.a_tree.remove_a_node(mymap.a_tree.root ,std::pair< int , int> (1, 42) );
+    //
+    mymap.a_tree.print2D( mymap.a_tree.root , 6);
+    // mymap.erase
     		//  a_tree.print2D(a_tree.root,6);		 
 
     // mymap.insert(std::pair< int , int> (65, 423111) );
