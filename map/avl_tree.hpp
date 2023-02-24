a060836dfa50ac2d65094fb65d9aa48ba9ead228
Anas TABITI <atabiti@e1r6p1.1337.ma>
2023-02-24 12:28:19 +0100

    working on avl is fixed

--- map/avl_tree.hpp
+++ map/avl_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 11:08:00 by atabiti           #+#    #+#             */
-/*   Updated: 2023/02/24 11:48:52 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/02/24 12:27:27 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -154,26 +154,25 @@ public:
       }
        else if (root_node->right == NULL) 
       {
-        node_p tmp = root_node->left;
-
-        alloc_it.destroy(root_node);
-        return tmp;
+          node_p tmp = root_node->left;
+          alloc_it.destroy(root_node);
+          return tmp;
       }
       else
       {
         
-        // if(root_node->left == NULL && root_node->right == NULL)
-        //             return NULL;
-        // node_p tmp = max_node(root_node);
-        // alloc_it.construct(root_node, tmp->value);
-        // node_p tmp_left = root_node->left->left;
-        // // std::cout  << tmp_left->value.first << std::endl;
-        // alloc_it.destroy(root_node->left);
-        // root_node->left = tmp_left;
-        node_p tmp = min_node(root_node->right);
-          // root_node->value = tmp->value;
-          alloc_it.construct(root_node, tmp->value);  // do initialise left and right in node constructor to avoid this issue here <<<<--------
-          root_node->right = remove_a_node(root_node->right, tmp->value);
+        if(root_node->left == NULL && root_node->right == NULL)
+                    return NULL;
+        node_p tmp = max_node(root_node);
+        alloc_it.construct(root_node, tmp->value);
+        node_p tmp_left = root_node->left->left;
+        // std::cout  << tmp_left->value.first << std::endl;
+        alloc_it.destroy(root_node->left);
+        root_node->left = tmp_left;
+          //   node_p tmp = min_node(root_node->right);
+          // // root_node->value = tmp->value;
+          // alloc_it.construct(root_node, tmp->value);  // do initialise left and right in node constructor to avoid this issue here <<<<--------
+          // root_node->right = remove_a_node(root_node->right, tmp->value);
           
       }
       
@@ -244,9 +243,7 @@ public:
       root_node->right = right_Rotation(root_node->right); 
       return left_Rotation(root_node);
     }
-
-    
-  
+      
     return root_node;
   }
 
@@ -355,6 +352,9 @@ public:
 
     return root_node;
   }
+
+
+
 
   void insert(T x)
     { 
